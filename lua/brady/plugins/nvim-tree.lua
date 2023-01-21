1d3381dc43d3ded0f3196d8480d13ab63fe494f6
bradybutcher <bbutcher@wccnet.edu>
2023-01-20 22:33:41 -0500

    Add existing files prior to Github push

---
+++ lua/brady/plugins/nvim-tree.lua
@@ -0,0 +1,33 @@
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+  return
+end
+
+-- recommended settings
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+nvimtree.setup({
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = " ",
+          arrow_open = " ",
+        },
+      },
+    },
+  },
+  -- disable window_picker for
+  -- explorer to work well with
+  -- window splits
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false,
+      },
+    },
+  },
+})
