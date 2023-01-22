2f97643e06e3c9a7385d595fd7e91d8b4510dfdf
bradybutcher <bbutcher@wccnet.edu>
2023-01-21 21:14:59 -0500

    Changing keybinds for trouble

---
+++ lua/brady/plugins/trouble.lua
@@ -0,0 +1,28 @@
+local trouble_setup, trouble = pcall(require, "trouble")
+if not trouble_setup then
+	return
+end
+
+trouble.setup({
+	defaults = {
+		mappings = {
+			-- Lua
+			vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true }),
+			vim.keymap.set(
+				"n",
+				"<leader>xw",
+				"<cmd>TroubleToggle workspace_diagnostics<cr>",
+				{ silent = true, noremap = true }
+			),
+			vim.keymap.set(
+				"n",
+				"<leader>xd",
+				"<cmd>TroubleToggle document_diagnostics<cr>",
+				{ silent = true, noremap = true }
+			),
+			vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true }),
+			vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true }),
+			vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true }),
+		},
+	},
+})
