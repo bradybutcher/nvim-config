4256e0322f041151fcb9698a3e9514f206a1fbb7
bradybutcher <bbutcher@wccnet.edu>
2023-01-22 20:48:47 -0500

    Changing colorscheme

--- lua/brady/plugins/lualine.lua
+++ lua/brady/plugins/lualine.lua
@@ -1,32 +1,32 @@
 local status, lualine = pcall(require, "lualine")
 if not status then
-  return
+	return
 end
 
 local lualine_nightfly = require("lualine.themes.nightfly")
+local lualine_tokyonight = require("lualine.themes.tokyonight")
 
 local new_colors = {
-  blue = "#65D1FF",
-  green = "#3EFFDC",
-  violet = "#FF61EF",
-  yellow = "#FFDA7B",
-  black = "#000000",
+	blue = "#65D1FF",
+	green = "#3EFFDC",
+	violet = "#FF61EF",
+	yellow = "#FFDA7B",
+	black = "#000000",
 }
 
-lualine_nightfly.normal.a.bg = new_colors.blue
-lualine_nightfly.insert.a.bg = new_colors.green
-lualine_nightfly.visual.a.bg = new_colors.violet
-lualine_nightfly.command = {
-  a = {
-    gui = "bold",
-    bg = new_colors.yellow,
-    fg = new_colors.black,
-  },
+lualine_tokyonight.normal.a.bg = new_colors.blue
+lualine_tokyonight.insert.a.bg = new_colors.green
+lualine_tokyonight.visual.a.bg = new_colors.violet
+lualine_tokyonight.command = {
+	a = {
+		gui = "bold",
+		bg = new_colors.yellow,
+		fg = new_colors.black,
+	},
 }
-
 
 lualine.setup({
-  options = {
-    theme = lualine_nightfly
-  }
+	options = {
+		theme = lualine_nightfly,
+	},
 })
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -206,6 +206,11 @@ _G.packer_plugins = {
     path = "/Users/bradybutcher/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["tokyonight.nvim"] = {
+    loaded = true,
+    path = "/Users/bradybutcher/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    url = "https://github.com/folke/tokyonight.nvim"
+  },
   ["trouble.nvim"] = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0" },
     loaded = true,
